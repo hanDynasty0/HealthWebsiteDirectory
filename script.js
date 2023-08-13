f8abd338fbf51dc1d9562e7701d178bd8e87026f
hanDynasty0 <142040454+hanDynasty0@users.noreply.github.com>
2023-08-13 19:05:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const sites = [
   "https://myfridgefood.com/",
   "https://www.rainymood.com/",
   "https://educationalgames.nobelprize.org/educational/medicine/bloodtypinggame/index.php",
-  "https://therealfoodrds.com/"
+  "https://therealfoodrds.com/",
+  "https://www.healthline.com/",
+  "https://www.thekitchn.com/collection/cooking-school",
+  "https://mapsofthemind.com/2019/10/30/6-music-playlists-psilocybin-journeys/",
+  "https://basicswithbabish.co/episodes"
 ]
 document.getElementById("button").onclick = function(){
   var i = Math.floor(Math.random()*sites.length);
