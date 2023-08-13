5c91fc1f6a2c648976f512aed6d52d6e0c59a92d
hanDynasty0 <142040454+hanDynasty0@users.noreply.github.com>
2023-08-13 14:22:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const sites = [
+  "https://musclewiki.com/",
+  "https://sleepopolis.com/calculators/sleep/",
+  "https://myfridgefood.com/",
+  "https://www.rainymood.com/",
+  "https://educationalgames.nobelprize.org/educational/medicine/bloodtypinggame/index.php",
+  "https://therealfoodrds.com/"
+]
 document.getElementById("button").onclick = function(){
-  window.location.href = "healthSite1.html";
+  var i = Math.floor(Math.random()*sites.length);
+  var site = sites[i];
+  window.location.href = site;
 }
